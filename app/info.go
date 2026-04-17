90a18c76c0e8898b3de0d54ebcbd9be755ba51d4
MonarchRyuzaki <shivamganguly2004@gmail.com>
2026-04-17 11:48:01 +0530

    Feat: master replid and repl offset

--- app/info.go
+++ app/info.go
@@ -3,13 +3,17 @@ package main
 import "fmt"
 
 type ServerInfo struct {
-	role string
+	role               string
+	master_replid      string
+	master_repl_offset int
 }
 
 var serverInfo = ServerInfo{}
 
 func NewServerInfo(role string) *ServerInfo {
 	serverInfo.role = role
+	serverInfo.master_replid = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb"
+	serverInfo.master_repl_offset = 0
 	return &serverInfo
 }
 
@@ -18,5 +22,5 @@ var ServerHandler = map[string]func(*ServerInfo, []Value) Value{
 }
 
 func handleInfo(s *ServerInfo, args []Value) Value {
-	return Value{Type: BULK, Bulk: fmt.Sprintf("role:%s", s.role)}
+	return Value{Type: BULK, Bulk: fmt.Sprintf("role:%s\nmaster_replid:%s\nmaster_repl_offset:%v", s.role, s.master_replid, s.master_repl_offset)}
 }
